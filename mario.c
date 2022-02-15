0e9444f0e5d976b9c1c517ec2bc1957ed4755f65
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-02-14 22:53:00 -0300

    Add files via upload

--- mario.c
+++ mario.c
@@ -6,23 +6,29 @@ int main () {
     printf("**   Bem vindo ao nosso jogo Mario Brother **\n");
     printf("*********************************************\n\n\n");
 
-	int i, j, n, chute;
+	int n;
+	do
+	{
 	printf("Digite o tamanho da altura: ");
 	scanf("%d", &n);
-
-	for (i = 1; i <= n; i++){
-        for(j = n - i; j >= 1; j --)
-            printf(" ");
-		for(j = 1 ; j <= i; j++)
-			printf("#");
-	
+	}
+	while(n < 1 || n > 8);
+    
+	for (int i =0 ; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if(i + j < n -1)
+			{
+				printf(" ");
+			}
+			else{
+				printf("#");
+			}
+		}
+		
 	printf("\n");
+	}
+	
 }	
 
-if(n == 8){
-    printf("Você digitou maior que 8");
-}else{
-    printf("Você digitou menor que 1");
-}
-
-}
